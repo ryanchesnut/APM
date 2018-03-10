46d61a75b19a0050a2687d7922614fb4b16fdc4b
ryan <ryanjchesnut@gmail.com>
2018-03-10 00:20:11 -0500

    added showImage logic
    added show image logic

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -8,6 +8,9 @@ import { Component } from '@angular/core';
 
 export class ProductListComponent {
     pageTitle: string = 'Product List';
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    showImage: boolean = false;
     products: any[] = [
         {
             'productId': 1,
@@ -61,4 +64,8 @@ export class ProductListComponent {
         }
 
     ];
+    toggleImage(): void {
+        this.showImage = !this.showImage;
+    }
+
 }
