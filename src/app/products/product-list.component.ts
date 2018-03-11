27f9a567abf74d01072739f67bbf96bbf585622e
ryan <ryanjchesnut@gmail.com>
2018-03-10 20:43:33 -0500

    filter
    Added product filter

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -9,11 +9,21 @@ import { IProduct } from './product';
 
 
 export class ProductListComponent implements OnInit {
+
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
-    listFilter: string = 'cart';
+
+    _listFilter: string;
+    get listFilter() {
+        return this._listFilter;
+    }
+    set listFilter(value: string) {
+       this._listFilter = value;
+       this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+    }
+    filteredProducts: IProduct[];
     products: IProduct[] = [
         {
             'productId': 1,
@@ -72,6 +82,17 @@ export class ProductListComponent implements OnInit {
     }
     ngOnInit(): void {
         console.log('In OnInit');
+    }
+
+    constructor() {
+        this.filteredProducts = this.products;
+        this.listFilter = 'cart';
+    }
+
+    performFilter(filterBy: string): IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) =>
+            product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
     }
 
 }
