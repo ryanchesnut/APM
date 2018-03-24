af88b2c17cbc2c7493c5a115241e3e3ccb28b597
ryan <ryanjchesnut@gmail.com>
2018-03-23 22:03:00 -0400

    Refactored, created project module
    Created project module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,43 +3,31 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { StarComponent } from './shared/star.component';
 import { ProductService } from './products/product.service';
 import { HttpClientModule } from '@angular/common/http';
-import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
 import { RouterModule } from '@angular/router';
 import { ProductGuardService } from './products/product-guard.service';
+import { ProductModule } from './products/product.module';
 
 @NgModule({
   declarations: [AppComponent,
-                ProductListComponent,
-                StarComponent,
-                ConvertToSpacesPipe,
-                ProductDetailComponent,
-                WelcomeComponent],
+    WelcomeComponent],
 
   imports: [
-            BrowserModule,
-            HttpClientModule,
-            FormsModule,
-            RouterModule.forRoot([
-              { path: 'products', component: ProductListComponent },
-              { path: 'products/:id',
-                  canActivate: [ ProductGuardService ],
-                  component: ProductDetailComponent },
-              { path: 'welcome', component: WelcomeComponent },
-              { path: '', redirectTo: 'welcome', pathMatch: 'full'},
-              { path: '**', redirectTo: 'welcome', pathMatch: 'full'}
-            ], {useHash: true})
-           ],
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+    ]),
+    ProductModule
+  ],
 
   providers: [
-              ProductService,
-              ProductGuardService,
-            ],
+  ],
 
   bootstrap: [AppComponent]
 })
